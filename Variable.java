8a3c4f5aae2bba46244d039eb5b014c41f197f28
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-28 17:42:56 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,21 @@
+class Variable
+{
+public static void main(String args[])
+{
+char number=5;
+switch(number)
+{
+case 3:
+System.out.println("smallest number");
+break;
+case 4:
+System.out.println("second largest number");
+break;
+case 5:
+System.out.println("largest number");
+break;
+default:
+System.out.println("invalid number");
+}
+}
+}
